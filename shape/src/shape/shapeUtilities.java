99cbfb54f40f9b3107670ea9f5bf6fb0e2f081ce
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2017-10-17 13:14:15 -0400

    shape utilities changes

--- shape/src/shape/shapeUtilities.java
+++ shape/src/shape/shapeUtilities.java
@@ -1,5 +1,37 @@
+import java.util.Random;
 package shape;
 
-public class shapeUtilities {
-
+public class shapeUtilities 
+{
+	public static shape getRandomShape()
+	{
+		random r = new Random();
+		
+		int x = r.nextInt(3);
+		switch(x)
+		{
+			case 0:
+				return new circle(r.nextInt(100)+1);
+			case 1:
+				return new rectangle()
+		}
+	}
+	public static double sumPerimeter(shape[] shapeArr)
+	{
+		int sum = 0;
+		for(int x=0; x<shapeArr.length(); x++)
+		{
+			sum = sum + shapeArr[x].perimeter;
+		}
+		return sum;
+	}
+	public static double sumArea(shape[] shapeArr)
+	{
+		int sum = 0;
+		for(int x=0; x<shapeArr.length(); x++)
+		{
+			sum = sum + shapeArr[x].area;
+		}
+		return sum;
+	}
 }
