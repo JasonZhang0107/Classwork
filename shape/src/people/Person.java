38138f7d41dad4d69ef682b727272d8ca7f9d271
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2017-10-20 13:14:02 -0400

    aa

---
+++ shape/src/people/Person.java
@@ -0,0 +1,10 @@
+package people;
+
+public abstract class Person
+{
+	public String dateOfBirth;
+	public String firstName;
+	public String lastName;
+	public abstract int calculateAge();
+	public abstract String toString();
+}
---
+++ shape/src/people/Student.java
@@ -0,0 +1,13 @@
+package people;
+
+public class Student extends Person
+{
+	public String[] classes;
+	public String school;
+	public double OSIS;
+	public int gradeLevel;
+	public double GPA;
+	
+	public Student()
+	
+}
